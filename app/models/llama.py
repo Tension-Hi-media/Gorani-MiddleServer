54db1d6d7af51d33d75319cfc47de874ecb432e3
JDH <yjkl0516@gmail.com>
2025-02-13 18:14:59 +0900

    .

--- app/models/llama.py
+++ app/models/llama.py
@@ -5,7 +5,7 @@ dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for
 load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name="unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit", # YOUR MODEL YOU USED FOR TRAINING
+    model_name="unsloth/Llama-3.2-3B-Instruct-bnb-4bit", # YOUR MODEL YOU USED FOR TRAINING
     max_seq_length = max_seq_length,
     device_map="auto",
     dtype = dtype,
